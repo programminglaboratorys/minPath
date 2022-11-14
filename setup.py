95dc676133642d4ef7fb9c96683396e0da1604d1
programminglaboratorys <107296738+programminglaboratorys@users.noreply.github.com>
2022-11-14 13:19:48 -0800

    update 0.1.3 minpath setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ classifiers = [
   'Programming Language :: Python :: 3',
   'License :: OSI Approved :: MIT License'
 ]
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = 'Alawi Hussein Adnan Al Sayegh'
 __description__ = 'imaginary path aka MiniPath'
 __license__ = """
